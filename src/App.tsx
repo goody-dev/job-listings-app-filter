512715796754d87860145806921134df69958952
Goodness Okanlawon <102490495+CodeEvolver@users.noreply.github.com>
2023-12-24 02:39:18 +0100

    started managing selected cartegories with state.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,64 @@
 import './App.css';
-
+import { useEffect, useState } from 'react';
 import Listings from './Components/Listings';
 import jobListings from './assets/data.json';
 import CartegoriesTab from './Components/CartegoriesTab';
 
+interface Cartegories {
+  HTML:boolean;
+  CSS:boolean;
+  JavaScript:boolean;
+  Frontend: boolean;
+  Senior: boolean;
+  Fullstack: boolean;
+  Midweight:boolean;
+  Python:boolean;
+  React:boolean;
+  Junior:boolean;
+  Sass:boolean;
+  Ruby:boolean;
+  Backend:boolean;
+  RoR:boolean;
+  Vue:boolean;
+  Django:boolean;
+}
 
 function App() {
+  const [cartegories, setCartegories] = useState<Cartegories>({
+    HTML:true, 
+    CSS:true, 
+    JavaScript:true,
+    Frontend: true,
+    Senior: false,
+    Fullstack: false,
+    Midweight:true,
+    Python:true,
+    React:true,
+    Junior:false,
+    Sass:true,
+    Ruby:false,
+    Backend:true,
+    RoR:true,
+    Vue:false,
+    Django:true
+  })
+
+  const selectedCartegories = () => {
+    const selectedCartegories: string[] = [];
+    Object.entries(cartegories).map(([cart, value])=> value===true && selectedCartegories.push(cart));
+    //alert(selectedCartegories);
+    return selectedCartegories;
+  }
+
+  useEffect (()=> {
+    selectedCartegories()
+  }, [cartegories])
+
   return (
     <div>
       <div className='header'>
       </div>
-      <CartegoriesTab />
+      <CartegoriesTab cartegories={selectedCartegories()} />
       <Listings jobs={jobListings} />
     </div>
   );
--- src/Components/CartegoriesTab.tsx
+++ src/Components/CartegoriesTab.tsx
@@ -1,11 +1,13 @@
 import CartegoryTab from "../Utilities/CartegoryTab";
 
-const CartegoriesTab = () => {
-  const selCart: string[] = ["Frontend", "React", "Midweight", "CSS", "ROR"]
+interface CartegoriesTabComponent {
+  cartegories: string[];
+}
+const CartegoriesTab:React.FC<CartegoriesTabComponent> = ({cartegories}) => {
   return (
     <div className='cartegories-tab'>
       <div className="tab-cartegories-div">
-        {selCart.map((cartegory:string, id) => <CartegoryTab key={id} cartegory={cartegory} />)}
+        {cartegories.map((cartegory:string, id:number) => <CartegoryTab key={id} cartegory={cartegory} />)}
       </div>
       <button id="clear-button">Clear</button>
     </div>
